8214cc59bac210c20bdb5310a569bfc26c14732a
HenriqueMertins <103587526+HenriqueMertins@users.noreply.github.com>
2024-10-16 23:21:28 +0000

    width

--- HACKAWEEN/src/Components/Button/style.tsx
+++ HACKAWEEN/src/Components/Button/style.tsx
@@ -3,6 +3,6 @@ import styled from 'styled-components'
 export const MyButton = styled.button `
     color: black;
     background-color: purple;
-    weight: 300px;
+    width: 300px;
     height: 100px;
 `
